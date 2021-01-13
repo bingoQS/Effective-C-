47b8061e6d646229c28954912fd593673076e48e
qbb <845514069@qq.com>
2021-01-13 11:53:58 +0800

    条款20代码

--- src/20.cpp
+++ src/20.cpp
@@ -37,8 +37,15 @@ bool validateStudent(const Student& s);
 
 class Window {
 public:
-	string name() const;               //返回窗口名称
+	string name() const               //返回窗口名称
+	{
+		return m_Name;
+	}
+
 	virtual void display() const;      //显示窗口和其内容
+
+private:
+	string m_Name;
 };
 
 class WindowWithScrollBars :public Window {
@@ -46,9 +53,20 @@ public:
 	virtual void display() const;
 };
 
+void printNameAndDisplay(Window w)
+{
+	cout << w.name();
+	w.display();
+}
 int main()
 {
 	Student plato;
 	bool platoIsOk = validateStudent(plato);
+
+	WindowWithScrollBars wwsb;
+	printNameAndDisplay(wwsb);
+
+	int a=10 ;
+	int * const pa = &a;//引用本质
 	return 0;	
 }
